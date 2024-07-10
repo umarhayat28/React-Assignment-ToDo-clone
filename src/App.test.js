6fc74c71f2218cf8a0067084082111ce0a4d89c0
chessmanpk <chessmanpk1@gmail.com>
2024-07-10 11:57:00 +0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App from './src/App';
 
 describe('App', () => {
   test('renders App component', () => {
